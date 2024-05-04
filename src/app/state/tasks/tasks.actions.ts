b8210cd8eaf24733506221bed94d7b785119b358
Luis Fernando Fz <luis.fernan.fz@gmail.com>
2024-05-03 19:23:11 -0600

    chore(vscode): add custom vscode settings

--- src/app/state/tasks/tasks.actions.ts
+++ src/app/state/tasks/tasks.actions.ts
@@ -1,4 +1,4 @@
-import { Action, createAction, props } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
 import { Task } from '../../interfaces/task.interface';
 
 export const add = createAction('[TASK] Add', props<{ task: Task }>());
